d2a4064127d93e37395a49977311a4a00b8735a9
Jinrui <jerry.chenj@outlook.com>
2024-11-18 23:34:11 +0800

    remove basic folder

--- basic/index1.js
+++
@@ -1,8 +0,0 @@
-import readline from "node:readline";
-
-const repeatCount = process.stdout.rows - 2;  // process.stdout.rows 终端可显示的行数
-const blank = repeatCount > 0 ? "\n".repeat(repeatCount) : "";
-console.log(blank);
-
-readline.cursorTo(process.stdout, 0, 0);
-readline.clearScreenDown(process.stdout);
